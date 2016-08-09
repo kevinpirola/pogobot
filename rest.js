82a0ec76728e3c3af15bbeab06f6625a7c688ef6
Laggia Gianmarco <glaggia@siav.net@vdidevlx13.siav.net>
2016-08-09 05:10:01 +0200

    First REST implementation

---
+++ rest.js
@@ -0,0 +1,9 @@
+/**
+ * This class is used to run a set of RESTful API
+ *
+ * @author Gianmarco Laggia
+ * @since 09/08/2016
+ *
+ */
+var express = require('express'); // call express
+var app = express();
