9b7cf016e03229a4677279d6f11f4d37edd21c7c
glaggia-larus <gianmarco.laggia@larus-ba.it>
2016-08-10 05:43:21 +0200

    Fixed little problems

--- rest.js
+++ rest.js
@@ -63,13 +63,6 @@ var port = process.env.PORT || 8080; // set the port
 // =============================================================================
 var router = express.Router();
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function (req, res) {
-    res.json({
-        message: 'hooray! welcome to our api!'
-    });
-});
-
 /**
  * REST SERVICE TO OBTAIN A TOKEN FROM GOOGLE LOGIN SERVICE
  */
