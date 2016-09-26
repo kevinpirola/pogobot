fc8388347499fd109dd2c56eca4b79d77a174605
Alberto D'Este <alberto.deste@larus-ba.it>
2016-09-26 18:18:10 +0200

    Added basic distribution progress bar

--- pogobot-frontend/app/scripts/controllers/gyms.js
+++ pogobot-frontend/app/scripts/controllers/gyms.js
@@ -12,9 +12,25 @@ angular.module('pogobotFrontendApp')
 
         var self = this;
         self.gyms = [];
+	self.distribution = [{class: 'blue-bar'}, {class: 'red-bar'}, {class: 'yellow-bar'}];
 
         Gym.get(null, function (res) {
             self.gyms = res.data;
+	    
+	    self.distribution[0].value = 0;
+	    self.distribution[1].value = 0;
+	    self.distribution[2].value = 0;
+
+	    angular.forEach(self.gyms, function(value, key){
+		if(value.GD_OWNER_TEAM === 1){
+		    self.distribution[0].value += (1 / self.gyms.length) * 100;	
+		} else if (value.GD_OWNER_TEAM === 2) {
+		    self.distribution[1].value += (1 / self.gyms.length) * 100;	
+		} else {
+		    self.distribution[2].value += (1 / self.gyms.length) * 100;	
+		}
+	    });
+console.log(self.distribution);
         });
 
     }]);
